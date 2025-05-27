e9f2b28bea8effb648243d3c386f93e1b1ee1def
panjigading <panjigading@student.ub.ac.id>
2025-05-27 11:46:19 +0700

    Add post image preview if exist

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -11,12 +11,21 @@
         <ul class="border border-b-0">
             @foreach ($posts as $post)
             <li class="p-6 border-b">
-                <p class="mb-2">
+                <p class="mb-4">
                     <a class="underline" href="{{ route('user', ['username' => $post->username]) }}">u/{{ $post->username }}</a>
                     - {{ $post->display_name }}
                 </p>
-                <h2 class="mb-2"><a class="font-semibold" href="{{ route('post', ['id' => $post->id]) }}"">{{ $post->title }}</a></h2>
-                <p>{{ Str::limit($post->body, 200, '...', preserveWords: true) }}</p>
+                <div class="flex gap-4">
+                    @if ($post->image)
+                    <div  class="h-full max-w-30">
+                        <img src="{{ Storage::url($post->image) }}" class="rounded">
+                    </div>
+                    @endif
+                    <div class="flex-1">
+                        <h2 class="mb-2"><a class="font-semibold" href="{{ route('post', ['id' => $post->id]) }}"">{{ $post->title }}</a></h2>
+                        <p>{{ Str::limit($post->body, 200, '...', preserveWords: true) }}</p>
+                    </div>
+                </div>
             </li>
             @endforeach
         </ul>
