320dc785eb238d7c72d86d444e75d7176974ecf0
panjigading <panjigading@student.ub.ac.id>
2025-06-03 11:35:55 +0700

    Make cursor as pointer in confirm_delete.blade.php

--- resources/views/user/confirm_delete.blade.php
+++ resources/views/user/confirm_delete.blade.php
@@ -3,8 +3,8 @@
         <h1 class="text-2xl text-center mb-4">Beneran mau hapus Akun?</h1>
         <form action="{{ route('user.confirm_delete') }}" method="POST" class="flex gap-2 w-fit mx-auto ">
         @csrf
-            <a href="{{ route('posts') }}" class="p-1 px-4 bg-black text-white rounded-sm mr-4">Ga Jadi</a>
-            <input type="submit" value="Iya" class="p-1 px-4 border rounded-sm mr-4 border-red-500 text-red-500">
+            <a href="{{ route('posts') }}" class="p-1 px-4 bg-black text-white rounded-sm mr-4 cursor-pointer">Ga Jadi</a>
+            <input type="submit" value="Iya" class="p-1 px-4 border rounded-sm mr-4 border-red-500 text-red-500 cursor-pointer">
         </form>
     </div>
 </x-layout>
